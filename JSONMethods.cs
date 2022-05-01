aed89ac21c3252249000d7d773d97d4338a3c627
Rohan-Sudagar <66852579+Rohan-Sudagar@users.noreply.github.com>
2022-05-01 12:31:02 -1000

    Add files via upload

---
+++ JSONMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace JSONPackage
+{
+    public class JSONMethods
+    {
+        public static void WriteJSONFile(List<String> data, string fileName)
+        {
+            string json = JsonSerializer.Serialize(data);
+            File.WriteAllText(fileName, json);
+            Console.WriteLine($"A file named {fileName} was created with JSON data.");
+        }
+    }
+}
